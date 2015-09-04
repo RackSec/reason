85097a4376d3bc07f3799dba039b2894dbf67d52
Laurens Van Houtven <_@lvh.io>
2015-09-04 16:29:02 -0700

    Add tests for prefix?

--- test/core_test.cljs
+++ test/core_test.cljs
@@ -2,3 +2,8 @@
   (:require
    [cljs.test :refer-macros [deftest is]]
    [reason.core :as rc]))
+
+(deftest prefix?-test
+  (is (rc/prefix? "abc" "abc"))
+  (is (rc/prefix? "abc" "a"))
+  (is (not (rc/prefix? "abc" "xyz"))))
