153a36f2a47e88a6d39ca947a0d6fa42f907019d
Laurens Van Houtven <_@lvh.io>
2015-09-08 10:54:10 -0700

    0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reason "0.1.0"
+(defproject reason "0.2.0-SNAPSHOT"
   :description "Library for producing predicates from textual user input."
   :url "https://github.com/racksec/reason"
   :license {:name "Eclipse Public License"
