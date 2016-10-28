a03f7db5e227a9109e282eaff1f5247be6ac18f9
lvh <_@lvh.io>
2016-10-28 11:39:33 -0500

    Version 0.10.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reason "0.9.0"
+(defproject reason "0.10.0-SNAPSHOT"
   :description "Library for producing predicates from textual user input."
   :url "https://github.com/racksec/reason"
   :license {:name "Eclipse Public License"
